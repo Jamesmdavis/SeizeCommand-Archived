be1329bfb33044c774ec387cc23c40ea5a69c3bc
James Davis <jamesmdavis95@gmail.com>
2019-07-11 10:39:20 -0700

    Damage Senders
    - AbstractDamageSender
    - ProjectileDamageSender

---
+++ Seize Command/Assets/Scripts/DamageSenders/AbstractDamageSender.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SeizeCommand.DamageSenders
+{
+    public abstract class AbstractDamageSender : MonoBehaviour
+    {
+        
+    }
+}
