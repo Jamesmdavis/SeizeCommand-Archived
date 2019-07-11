0c166cefb2bafdbd07edf6d3898a55cf12a5575e
James Davis <jamesmdavis95@gmail.com>
2019-07-11 10:11:04 -0700

    Attack
    - Abstract Attack
    - Player Attack

---
+++ Seize Command/Assets/Scripts/Attack/AbstractAttack.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SeizeCommand.Attack
+{
+    public abstract class AbstractAttack : MonoBehaviour
+    {
+        protected abstract void Attack();
+    }
+}
