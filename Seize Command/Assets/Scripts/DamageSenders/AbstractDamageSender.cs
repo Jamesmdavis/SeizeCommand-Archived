3e6ffe3c65bbfdc71ee8fe0dbfc683bbcc7711d0
James Davis <jamesmdavis95@gmail.com>
2019-07-30 12:40:03 -0700

    Update AbstractDamageSender.cs

--- Seize Command/Assets/Scripts/DamageSenders/AbstractDamageSender.cs
+++ Seize Command/Assets/Scripts/DamageSenders/AbstractDamageSender.cs
@@ -18,7 +18,7 @@ namespace SeizeCommand.DamageSenders
 
         protected virtual void OnTriggerEnter2D(Collider2D coll)
         {
-            if(coll.GetComponent<Rigidbody2D>() != null)
+            if(coll.GetComponent<Rigidbody2D>() != null || !coll.isTrigger)
             {
                 if(coll.gameObject.GetComponent(typeof(IDamageable)))
                 {
