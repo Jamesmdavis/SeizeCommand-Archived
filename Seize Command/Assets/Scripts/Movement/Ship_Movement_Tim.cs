df5768c5b83b6fa78b115f41820e4e662cc1a4de
Rhodomotor <39973538+Rhodomotor@users.noreply.github.com>
2019-08-18 22:06:53 -0700

    Ship Scene
    This has a ship scene that includes a square that will move up when you press "t" and back when you press "r"

---
+++ Seize Command/Assets/Scripts/Movement/Ship_Movement_Tim.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Movement_Tim : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    [SerializeField] private float thrustModifier = 1;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        bool Fwd = Input.GetKey("t");
+        bool Reverse = Input.GetKey("r");
+
+        ThrustForward(Fwd);
+        ThrustReverse(Reverse);
+    }
+
+    //TrustForward method will propel the craft in the up direction of it's axis when the "t" key is pressed
+    private void ThrustForward(bool Fwd)
+    {
+        if(Fwd  == true)
+        {
+            Vector2 force = transform.up * thrustModifier;
+            rb.AddForce(force);
+        }
+    }
+
+    //ThrustReverse method will create a force in the reverse direction when the "r" key is pressed
+    private void ThrustReverse(bool Reverse)
+    {
+        if( Reverse == true)
+        {
+            Vector2 force = -transform.up * thrustModifier;
+            rb.AddForce(force);
+        }
+    }
+}
