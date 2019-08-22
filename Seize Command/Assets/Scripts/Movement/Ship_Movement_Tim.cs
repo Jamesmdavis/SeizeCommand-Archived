c33f7dfb58244a9d80d278045a34b7a3e04d2b6b
Rhodomotor <39973538+Rhodomotor@users.noreply.github.com>
2019-08-21 22:12:50 -0700

    SpeedCap
    I added a speed cap to the ship.  Yay.

--- Seize Command/Assets/Scripts/Movement/Ship_Movement_Tim.cs
+++ Seize Command/Assets/Scripts/Movement/Ship_Movement_Tim.cs
@@ -6,6 +6,7 @@ public class Ship_Movement_Tim : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] private float thrustModifier = 1;
+    [SerializeField] private float maxSpeed = 10;
     // Start is called before the first frame update
     private void Start()
     {
@@ -16,16 +17,26 @@ public class Ship_Movement_Tim : MonoBehaviour
     private void Update()
     {
         bool Fwd = Input.GetKey("t");
-        bool Reverse = Input.GetKey("r");
+        bool Rvs = Input.GetKey("r");
+        float velocity = rb.velocity.magnitude;
 
         ThrustForward(Fwd);
-        ThrustReverse(Reverse);
+        ThrustReverse(Rvs);
+        VelocityCap(velocity);
+    }
+
+    private void VelocityCap(float speed)
+    {
+        if (speed >= maxSpeed)
+        {
+            rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+        }
     }
 
     //TrustForward method will propel the craft in the up direction of it's axis when the "t" key is pressed
-    private void ThrustForward(bool Fwd)
+    private void ThrustForward(bool forward)
     {
-        if(Fwd  == true)
+        if(forward  == true)
         {
             Vector2 force = transform.up * thrustModifier;
             rb.AddForce(force);
@@ -33,9 +44,9 @@ public class Ship_Movement_Tim : MonoBehaviour
     }
 
     //ThrustReverse method will create a force in the reverse direction when the "r" key is pressed
-    private void ThrustReverse(bool Reverse)
+    private void ThrustReverse(bool reverse)
     {
-        if( Reverse == true)
+        if( reverse == true)
         {
             Vector2 force = -transform.up * thrustModifier;
             rb.AddForce(force);
